3e5b79cb95fe1587a716c141b5881d7275b6e22a
Jisoo Park <cosmopark624@gmail.com>
2021-10-18 23:14:44 +0900

    add launch web button

--- assign/app/src/main/java/com/example/jisoopark20186668/MainActivity.java
+++ assign/app/src/main/java/com/example/jisoopark20186668/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import com.example.jisoopark20186668.databinding.ActivityMainBinding;
@@ -47,6 +48,11 @@ public class MainActivity extends AppCompatActivity {
         //startActivity(intent);
         resultLauncher.launch(intent);
 
+    }
+
+    public void showWebPage(View view){
+        Intent intentWeb = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.co.kr"));
+        startActivity(intentWeb);
     }
 
 
